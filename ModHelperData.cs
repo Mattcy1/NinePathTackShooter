e66a3b97bd49eaea50b87edb6acda993b110c20a
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-11-12 18:34:44 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace GlueGunnerFourthPath;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "39.2";
-    public const string Version = "1";
+    public const string Version = "1.1";
     public const string Name = "Nine Path Tack Shooter";
     public const string Description = "Today i decided to go a bit crazy so i made a another 9path tower!";
     public const string RepoOwner = "Mattcy1";
